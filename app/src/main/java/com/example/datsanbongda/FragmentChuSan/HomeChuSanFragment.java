4d73ffbd2101f2bc2a7aeeb53be72e0295de3556
legiabaoo <giabao.45378@gmailc.om>
2023-11-27 16:45:54 +0700

    giasan

--- app/src/main/java/com/example/datsanbongda/FragmentChuSan/HomeChuSanFragment.java
+++ app/src/main/java/com/example/datsanbongda/FragmentChuSan/HomeChuSanFragment.java
@@ -1,5 +1,6 @@
 package com.example.datsanbongda.FragmentChuSan;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,7 +8,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
+import com.example.datsanbongda.ActivityChuSan.ThongTinSanChuSanActivity;
 import com.example.datsanbongda.R;
 
 
@@ -18,7 +21,16 @@ public class HomeChuSanFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_chu_san, container, false);
+        View view = inflater.inflate(R.layout.fragment_home_chu_san, container, false);
+        LinearLayout thongtinsan = view.findViewById(R.id.thongtinsan);
+        thongtinsan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), ThongTinSanChuSanActivity.class);
+                startActivity(intent);
+            }
+        });
+        return view;
+
     }
 }
